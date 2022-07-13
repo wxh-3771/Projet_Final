7ecd6a28fdb561d7434f9dbbeef65ea8ee8cf929
wxh-3771 <benane.flora.1501@gmail.com>
2022-07-13 15:23:49 +0100

    questions

--- utilisateur/v/php/questions_par_categorie.php
+++ utilisateur/v/php/questions_par_categorie.php
@@ -21,6 +21,12 @@ require('../../c/questions/categorie.php');
       </head>
 
      <body>
+         
+        <?php 
+          require('../includes/navbar.php');
+        ?>
+      
+         
          <?php 
             while($t = $AllQuestions->fetch()) {
                ?>
@@ -39,6 +45,10 @@ require('../../c/questions/categorie.php');
          <br>
 
          <a href="poster.php?categorie=<?=$code_categorie;?>"> <button>Ajouter une nouvelle questions</button></a>
+          
+         <?php 
+          require('../includes/footer.php');
+        ?> 
 
          <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
          <script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.1/dist/js/bootstrap.bundle.min.js" integrity="sha384-fQybjgWLrvvRgtW6bFlB7jaZrFsaBXjsOMm/tB9LTS58ONXgqbR9W8oWht/amnpF" crossorigin="anonymous"></script>
